74682813760b5d23797a64c1e6f5d96c66445bbf
Adele Weisz <adiel.weisz@gmail.com>
2022-05-31 19:12:22 -0500

    adding to skills practice folder may fifth

---
+++ skills_practice.rb
@@ -0,0 +1,95 @@
+# number = 10
+
+# if number = 10 
+#   p 0 
+# else 
+#   p -1
+# end 
+
+# number = 7
+
+# if number < 10 
+#   p -1
+# elsif number > 10 
+#   p 1
+# else 
+#   p 0 
+# end 
+
+# number1 = 3
+# number2 = 9 
+
+# if number1 && number2 > 10 
+#   p 1
+# else 
+#   p 0 
+# end 
+
+# number = 28
+
+# if number < 10 
+#   p 9 
+# elsif number < 20 
+# p 19
+# elsif number < 30
+#   p 29
+# else 
+#   p -1 
+# end 
+ 
+# number = 8799
+
+# if number > 9000
+#   p 0
+# else 
+#   p 1
+# end 
+
+# number1 = 399
+# number2 = 6
+
+# if number1 || number2 > 10
+#   p 100
+# else
+#   p -100
+# end 
+
+# number = 387
+#  if number < 0 
+#   p 1776
+#  else 
+#   p 1979
+#  end 
+
+# number = 8
+
+# if number == 100
+#   p 100
+# elsif number == 99
+#   p 99
+# else
+#   p 0 
+# end
+
+# number1 = -45
+# number2 = 87
+
+# if number1 < 0 && number2 > 0
+#   p 1
+# else 
+#   p 0
+# end 
+
+number = 34
+
+if number > 80
+  p 5
+elsif number > 60
+  p 4
+elsif number > 40 
+  p 3
+elsif number > 20
+  p 2
+else
+  p 1 
+end 
